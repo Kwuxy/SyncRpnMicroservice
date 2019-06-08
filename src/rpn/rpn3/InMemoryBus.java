6385d23a232606a7fe8c353d2b4b4505674b4661
Vincent Guénin <guenin.vincent4@gmail.com>
2019-06-08 13:49:07 +0200

    [ADD] InMemoryBus implementation

---
+++ src/rpn/rpn3/InMemoryBus.java
@@ -0,0 +1,33 @@
+package rpn.rpn3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InMemoryBus implements Bus {
+    private Map<String, List<Consumer>> consumersByMessageType = new HashMap<>();
+
+    @Override
+    public void publish(Message message) {
+        List<Consumer> consumers = consumersByMessageType.get(message.messageType());
+
+        if(consumers == null) {
+            return;
+        }
+
+        consumers.forEach((consumer -> consumer.receive(message)));
+    }
+
+    @Override
+    public void subscribe(String messageType, Consumer consumer) {
+        List<Consumer> consumers = consumersByMessageType.get(messageType);
+
+        if(consumers == null) {
+            consumers = new ArrayList<>();
+            consumersByMessageType.put(messageType, consumers);
+        }
+
+        consumers.add(consumer);
+    }
+}
