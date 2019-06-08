48bc96a5edf994bfe1df75acc43fa33b0677011e
Vincent Guénin <guenin.vincent4@gmail.com>
2019-06-08 20:55:42 +0200

    [ADD] CalculatorConsumer

---
+++ src/CalculatorConsumer.java
@@ -0,0 +1,45 @@
+import rpn.rpn3.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class CalculatorConsumer implements Consumer {
+    private final Map<String, Stack<Double>> stacksByExpressionId;
+    private final Bus bus;
+
+    public CalculatorConsumer(Bus bus) {
+        this.bus = bus;
+        this.stacksByExpressionId = new HashMap<>();
+    }
+
+    @Override
+    public void receive(Message message) {
+        if(message instanceof TokenMessage) {
+            TokenMessage tokenMessage = (TokenMessage) message;
+            Stack<Double> stack = stacksByExpressionId.get(message.messageType());
+
+            if(stack == null) {
+                stack = new Stack<>();
+                stacksByExpressionId.put(message.messageType(), stack);
+            }
+
+            if(tokenIsNumber(tokenMessage.token())) {
+                stack.push(Double.valueOf(tokenMessage.token()));
+            }else{
+                bus.publish(new OperatorMessage(stack, tokenMessage.token(), tokenMessage.expressionId()));
+                stack.clear();
+            }
+        }
+    }
+
+    private boolean tokenIsNumber(String token) {
+        try {
+            Double.parseDouble(token);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/rpn/rpn3/OperatorMessage.java
@@ -0,0 +1,29 @@
+package rpn.rpn3;
+
+import java.util.Stack;
+
+public class OperatorMessage implements Message {
+    private final String MESSAGE_TYPE;
+
+    private final Stack<Double> operands;
+    private final String expressionId;
+
+    public OperatorMessage(Stack<Double> operands, String operator, String expressionId) {
+        this.operands = operands;
+        this.MESSAGE_TYPE = operator;
+        this.expressionId = expressionId;
+    }
+
+    @Override
+    public String messageType() {
+        return MESSAGE_TYPE;
+    }
+
+    public Stack<Double> operands() {
+        return operands;
+    }
+
+    public String expressionId() {
+        return expressionId;
+    }
+}
