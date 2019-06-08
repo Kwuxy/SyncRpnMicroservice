7c01dcc0c2a2bd61e51595cd3e8148eac2012031
Vincent Guénin <guenin.vincent4@gmail.com>
2019-06-08 22:34:34 +0200

    [UPD] What if there is not enough operands ?

--- src/rpn/rpn3/consumer/PlusConsumer.java
+++ src/rpn/rpn3/consumer/PlusConsumer.java
@@ -1,10 +1,12 @@
 package rpn.rpn3.consumer;
 
 import rpn.rpn3.bus.Bus;
+import rpn.rpn3.exception.NotEnoughOperandException;
 import rpn.rpn3.message.Message;
 import rpn.rpn3.message.OperatorMessage;
 import rpn.rpn3.message.ResultMessage;
 
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class PlusConsumer implements Consumer {
@@ -19,9 +21,14 @@ public class PlusConsumer implements Consumer {
         OperatorMessage operatorMessage = (OperatorMessage) message;
         Stack<Double> operands = operatorMessage.operands();
 
-        Double operand1 = operands.pop();
-        Double operand2 = operands.pop();
-        operands.push(operand1 + operand2);
+        try {
+            Double operand1 = operands.pop();
+            Double operand2 = operands.pop();
+            operands.push(operand1 + operand2);
+        }catch (EmptyStackException e) {
+            //throw new NotEnoughOperandException();
+            //Who is responsible for this ?
+        }
 
         bus.publish(new ResultMessage(operands, operatorMessage.expressionId()));
     }
