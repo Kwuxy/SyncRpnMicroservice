5ed84a1f770df573c464a1c6a09fa0fdc817e0d8
Vincent Guénin <guenin.vincent4@gmail.com>
2019-06-12 09:19:46 +0200

    [ADD] DropConsumer

--- src/main/java/rpn/rpn3/CLI.java
+++ src/main/java/rpn/rpn3/CLI.java
@@ -2,10 +2,7 @@ package rpn.rpn3;
 
 import rpn.rpn3.bus.InMemoryBus;
 import rpn.rpn3.consumer.*;
-import rpn.rpn3.consumer.operator.AbsConsumer;
-import rpn.rpn3.consumer.operator.DivideConsumer;
-import rpn.rpn3.consumer.operator.MinusConsumer;
-import rpn.rpn3.consumer.operator.PlusConsumer;
+import rpn.rpn3.consumer.operator.*;
 import rpn.rpn3.message.*;
 
 public class CLI {
@@ -21,6 +18,7 @@ public class CLI {
         bus.subscribe("-", new MinusConsumer(bus));
         bus.subscribe("ABS", new AbsConsumer(bus));
         bus.subscribe("/", new DivideConsumer(bus));
+        bus.subscribe("DROP", new DropConsumer(bus));
         bus.subscribe(ResultMessage.MESSAGE_TYPE, calculator);
         bus.subscribe(EndOfCalculationMessage.MESSAGE_TYPE, client);
 
@@ -34,5 +32,7 @@ public class CLI {
         client.calculate("1 ABS -2 ABS +");
 
         client.calculate("1 4 /");
+
+        client.calculate("1 3 + 5 DROP");
     }
 }
