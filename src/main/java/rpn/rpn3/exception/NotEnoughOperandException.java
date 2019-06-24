ff6df3bf435c9fc593ef92503b391f9b969b73e6
Vincent Guénin <guenin.vincent4@gmail.com>
2019-06-24 16:44:37 +0200

    [DEL] Custom exception

--- src/main/java/rpn/rpn3/exception/NotEnoughOperandException.java
+++
@@ -1,7 +0,0 @@
-package rpn.rpn3.exception;
-
-public class NotEnoughOperandException extends Exception {
-    public NotEnoughOperandException() {
-        super("Not enough operands to calculate operator '+'");
-    }
-}
