857b0c46578dee8f152329e33b1c25bbfe5b2260
Vincent Guénin <guenin.vincent4@gmail.com>
2019-06-24 17:01:32 +0200

    [ADD] Not enough operands for Plus

--- src/main/java/rpn/rpn3/consumer/operator/PlusConsumer.java
+++ src/main/java/rpn/rpn3/consumer/operator/PlusConsumer.java
@@ -2,7 +2,7 @@ package rpn.rpn3.consumer.operator;
 
 import rpn.rpn3.bus.Bus;
 import rpn.rpn3.consumer.Consumer;
-import rpn.rpn3.exception.NotEnoughOperandException;
+import rpn.rpn3.message.ErrorMessage;
 import rpn.rpn3.message.Message;
 import rpn.rpn3.message.OperatorMessage;
 import rpn.rpn3.message.ResultMessage;
@@ -21,16 +21,23 @@ public class PlusConsumer implements Consumer {
     public void receive(Message message) {
         OperatorMessage operatorMessage = (OperatorMessage) message;
         Stack<Double> operands = operatorMessage.operands();
+        Message messageToSend;
+        Integer operandCounter = 0;
 
         try {
             Double operand1 = operands.pop();
+            operandCounter++;
+
             Double operand2 = operands.pop();
+            operandCounter++;
+
             operands.push(operand1 + operand2);
+            messageToSend = new ResultMessage(operands, operatorMessage.expressionId());
         }catch (EmptyStackException e) {
-            //throw new NotEnoughOperandException();
-            //Who is responsible for this ?
+            messageToSend = new ErrorMessage("Not enough operands for operator '+'. Expected 2 got " + operandCounter,
+                    operatorMessage.expressionId());
         }
 
-        bus.publish(new ResultMessage(operands, operatorMessage.expressionId()));
+        bus.publish(messageToSend);
     }
 }
